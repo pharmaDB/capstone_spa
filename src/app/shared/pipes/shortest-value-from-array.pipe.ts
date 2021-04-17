bff55e42bc915c67bdaa745fbbdcfe0710703be9
Brett Bloethner <brett@bl8nr.com>
2021-04-17 12:59:01 -0600

    add comments to pipes

--- src/app/shared/pipes/shortest-value-from-array.pipe.ts
+++ src/app/shared/pipes/shortest-value-from-array.pipe.ts
@@ -2,13 +2,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 /**
  * ShortestValueFromArrayPipe
- * TODO: add comment
+ * Takes in an array of strings from a value provided on an Angular template and
+ * returns from the array, the string with the shortest length. Used to present
+ * a string of possible strings that must fit into a UI element. A tooltip should
+ * to display the rest of the possible strings.
  */
 @Pipe({
   name: 'shortestValueFromArray'
 })
 export class ShortestValueFromArrayPipe implements PipeTransform {
 
+  /**
+   * Pipe function transformation handler
+   * @param value String[]: all of the possible strings from which the shortest length will be returned
+   * @returns String: the shortest length string from the provided array of strings
+   */
   transform(value: string[]): string {
     return value.reduce((a, b) => a.length <= b.length ? a : b);
   }
