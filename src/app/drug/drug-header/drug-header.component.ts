61fb831ea5fd907d6b1630584a675346cd33d729
Brett Bloethner <brett@bl8nr.com>
2021-04-18 15:04:42 -0600

    add comments for the drug header component

--- src/app/drug/drug-header/drug-header.component.ts
+++ src/app/drug/drug-header/drug-header.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+/**
+ * DrugHeader Component
+ * The DrugHeader component is responsible for displaying the name of the Drug whose labels and patents are currently being viewed. It also
+ * provides convenient modals for viewing Drug Metadata.
+ */
 @Component({
   selector: 'app-drug-header',
   templateUrl: './drug-header.component.html',
   styleUrls: ['./drug-header.component.scss']
 })
-export class DrugHeaderComponent implements OnInit {
+export class DrugHeaderComponent {
   @Input() drug: any;
 
   constructor() { }
-
-  ngOnInit(): void {
-    console.log(this.drug);
-  }
 
 }
