341905637a3533b5e7887dfaf77cd3427cbe0792
Brett Bloethner <brett@bl8nr.com>
2021-04-18 15:29:37 -0600

    finish adding comments to the drug view components

--- src/app/drug/drug/drug.component.ts
+++ src/app/drug/drug/drug.component.ts
@@ -7,12 +7,20 @@ import * as _ from 'lodash';
 import {DrugService} from '../../shared/services/drug.service';
 import {DrugViewConfig, DrugViewMode} from '../drug-view-config.interface';
 
+/**
+ * DrugComponent
+ * The DrugComponent is the main component for the Drug view and coordinates data sharing and event handling across all its children
+ * components (DrugTimeline, DrugText and DrugHeader). Events from child components are observed and responded to and data is propagated
+ * down into the child components accordingly. Overall state of the Drug view is managed using params in the drugViewConfig object.
+ */
 @Component({
   selector: 'app-drug',
   templateUrl: './drug.component.html',
   styleUrls: ['./drug.component.scss']
 })
 export class DrugComponent implements OnInit {
+
+  // default empty drugViewConfig
   drugViewConfig: DrugViewConfig = {
     drugViewMode: DrugViewMode.none,
     inViewLabelOne: undefined,
@@ -21,17 +29,24 @@ export class DrugComponent implements OnInit {
     isPatentInView: false,
     inViewPatent: undefined
   };
+
   timelineItems: TimelineItem[] = [];
   isPageLoading = true;
   isTextLoading = false;
-  ndaNumber: string;
-  drug: any;
+  ndaNumber: string; // NDA number of Drug currently in-view
+  drug: any; // Raw data (from the API/DrugService) of the Drug currently in-view
 
   constructor(
     private route: ActivatedRoute,
     private drugService: DrugService,
   ) { }
 
+  /**
+   * onPatentClaimTagClicked handler
+   * handles events from the DrugText component once a patent claim tag is clicked by the user. Finds the appropriate patent and patent
+   * claim from the drug patents and triggers a drugViewConfig change accordingly.
+   * @param event: event emitted from the DrugText component, representing the user clicking on a patent claim tag
+   */
   onPatentClaimTagClicked(event: any): void {
     const patent = _.find(this.drug.drugPatents, (p: any) => p.patent_number === event.patent_number);
     const claim = _.find(patent.claims, (c: any) => c.claim_number === event.claim_number);
@@ -45,6 +60,11 @@ export class DrugComponent implements OnInit {
       inViewPatent: claim});
   }
 
+  /**
+   * onClasePatentViewClicked handler
+   * handles events from the DrugText component once the close button is clicked on the patent viewing window. Resets the patent and patent
+   * claim currently in-view and triggers a drugViewConfig change accordingly.
+   */
   onClosePatentViewClicked(): void {
     this.onDrugViewChange({
       drugViewMode: this.drugViewConfig.drugViewMode,
@@ -55,36 +75,60 @@ export class DrugComponent implements OnInit {
       inViewPatent: undefined});
   }
 
+  /**
+   * onDrugViewChange handler
+   * This handler is meant to be fired whenever a change is made that constitutes a change to the DrugViewConfig whether from this
+   * component or from a child component. It sets the new drugViewConfig and changes the view and data provided to DrugComponents children
+   * accordingly, this is also where the diff is executed.
+   * @param drugViewConfig: an object describing the new view to the rendered for the Drug view
+   */
   onDrugViewChange(drugViewConfig: DrugViewConfig): void {
+
+    // set new drugViewConfig
     this.drugViewConfig = drugViewConfig;
+
+    // if two labels are selected (ie the new drugViewConfig indicates a label diff)...
     if (drugViewConfig.inViewLabelOne && drugViewConfig.inViewLabelTwo) {
       const labelSectionDiffs: { name: string, scores?: any[], diff: any}[] = [];
+
+      // ...then iterate through each section of the label
       drugViewConfig.inViewLabelOne.data.sections.forEach((section: any) => {
+
+        // grab the alternate section from the opposing label
         const labelTwoSection = _.find(drugViewConfig.inViewLabelTwo.data.sections, (labelTwoSec: any) => {
           return labelTwoSec.name === section.name;
         });
 
+        // execute a diff on the two sections and clean it up to make it human readable
         const diffTool = new DiffMatchPatch();
         const diff = diffTool.diff_main(section.text, labelTwoSection.text);
         diffTool.diff_cleanupSemantic(diff);
-        console.log('labelTwoSection');
-        console.log(labelTwoSection);
+
+        // create a new section object with the diff data, patent claim scores and section name. store it.
         labelSectionDiffs.push({ name: section.name, scores: labelTwoSection.scores, diff });
       });
 
+      // store the diffs in the drugViewConfig, so they can be delivered to and presented by the DrugText component
       this.drugViewConfig.labelDiff = {
         sections: labelSectionDiffs
       };
     }
   }
 
+  /**
+   * On initialization of the DrugComponent, fetch the Drug data from the API and add each timeline and patent found
+   * to the timelineItems array to be provided to the DrugTimeline component for rendering
+   */
   ngOnInit(): void {
+    // get NDA number from current URL route
     this.ndaNumber = this.route.snapshot.params.NDANumber;
 
     this.drugService.getDrugByApplicationNumber(this.ndaNumber).subscribe((response) => {
-      // this.isPageLoading = false;
+
+      // store raw drug data for convenience in case needed for later features
       this.drug = response;
 
+      // for every drug label present, add a label timeline item to the timelineItems arr
       this.drug.drugLabels.forEach((label: any) => {
         const timelineLabel = {
           id: uuidv4(),
@@ -98,6 +142,7 @@ export class DrugComponent implements OnInit {
         this.timelineItems.push(timelineLabel);
       });
 
+      // for every drug patent present, add a patent timeline item to the timelineItems arr
       this.drug.drugPatents.forEach((patent: any) => {
         const timelinePatent = {
           id: uuidv4(),
@@ -111,13 +156,16 @@ export class DrugComponent implements OnInit {
         this.timelineItems.push(timelinePatent);
       });
 
-      //this.drugService.processDrugLabelDiffs(this.drug.drugLabels);
       this.isPageLoading = false;
     });
   }
 
 }
 
+/**
+ * Interface for objects used in the visJS timeline library. Fits the visJS timeline item construct but includes
+ * extra params specific to this web applications use case.
+ */
 export interface TimelineItem {
   id: string;
   content: string;
@@ -125,5 +173,5 @@ export interface TimelineItem {
   group: string;
   className: string;
   title: string;
-  // data: any;
+  data?: any;
 }
