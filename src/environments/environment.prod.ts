72faef9ba7fc9979502a11b88ed73012c1641d27
Brett Bloethner <brett@bl8nr.com>
2021-06-20 23:25:32 -0600

    change the default bulk download link to the zip file from the project github repo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   apiUrl: 'https://api.pharmadb.org',
   production: true,
-  bulkDownloadURL: 'http://ec2-35-170-81-159.compute-1.amazonaws.com/db2csv.csv.zip',
+  bulkDownloadURL: 'https://github.com/pharmaDB/scoring_data_processor/blob/main/resources/hosted_folder/db2csv.csv.zip?raw=true',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   apiUrl: 'https://api.pharmadb.org',
   production: false,
-  bulkDownloadURL: 'http://ec2-54-90-81-237.compute-1.amazonaws.com/db2csv.csv.zip',
+  bulkDownloadURL: 'https://github.com/pharmaDB/scoring_data_processor/blob/main/resources/hosted_folder/db2csv.csv.zip?raw=true',
 };
 
 /*
